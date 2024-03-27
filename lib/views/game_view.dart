967516ea79d5f2bffd7b24130bb3d2b82fd7f553
islambahaaa <bossislam55@gmail.com>
2024-03-27 05:22:54 +0200

    Add Pin Code Fields widget to GameView
    Integrate Pin Code Fields packadge widget insted of the custom item in the GameView screen.

--- lib/views/game_view.dart
+++ lib/views/game_view.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
-import 'package:guess_number/views/widgets/digit_item.dart';
-import 'package:guess_number/views/widgets/num_pad.dart';
-import 'package:numpad/numpad.dart';
+import 'package:pin_code_fields/pin_code_fields.dart';
 
 class GameView extends StatefulWidget {
   const GameView({super.key});
@@ -17,35 +16,64 @@ class _GameViewState extends State<GameView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Game'),
-      ),
-      // body: const Row(
-      //   children: [
-      //     DigitItem(),
-      //     DigitItem(),
-      //     DigitItem(),
-      //   ],
-      // )
-      body: Column(
-        children: [
-          Row(
-            children: [
-              DigitItem(text: num),
-              DigitItem(text: num),
-              DigitItem(text: num),
-            ],
-          ),
-          KeyPad(
-            pinController: pinController,
-            onChange: (String pin) {
-              num = pin;
+        appBar: AppBar(
+          title: const Text('Game'),
+        ),
+        // body: const Row(
+        //   children: [
+        //     DigitItem(),
+        //     DigitItem(),
+        //     DigitItem(),
+        //   ],
+        // )
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 32),
+              child: const PinFields(),
+            )
+          ],
+        ));
+  }
+}
 
-              setState(() {});
-            },
-          ),
-        ],
+class PinFields extends StatefulWidget {
+  const PinFields({
+    super.key,
+  });
+
+  @override
+  State<PinFields> createState() => _PinFieldsState();
+}
+
+class _PinFieldsState extends State<PinFields> {
+  final TextEditingController pinController = TextEditingController();
+  String previousText = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return PinCodeTextField(
+      appContext: context,
+      length: 3,
+      cursorHeight: 16,
+      enableActiveFill: true,
+      inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+      keyboardType: TextInputType.number,
+      controller: pinController,
+      pinTheme: PinTheme(
+        borderRadius: BorderRadius.circular(10),
+        shape: PinCodeFieldShape.box,
       ),
+      onChanged: (value) {
+        List<String> uniqueDigits = value
+            .split('')
+            .toSet()
+            .toList(); // Convert value to a list of unique digits
+        if (uniqueDigits.length < value.length) {
+          //cancel the last input of the user
+        }
+      },
     );
   }
 }
